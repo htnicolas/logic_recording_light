7305424ee020fbfa05e7efb72e2902a33a705c80
Nicolas HT <nutella50g@gmail.com>
2025-01-19 17:08:47 -0800

    First commit: client + server

---
+++ client.py
@@ -0,0 +1,62 @@
+# This program is to be run on the machine running Logic Pro X.
+# To setup recording light, go to Logic Pro X -> Settings -> Control Surfaces -> Setup -> New -> Recording Light
+import argparse
+
+from loguru import logger
+import rtmidi
+from pythonosc import udp_client
+
+IP_ADDRESS = "127.0.0.1"
+PORT = 5005
+client = udp_client.SimpleUDPClient(IP_ADDRESS, PORT)
+
+
+def send_midi_message_over_osc(message, data):
+    """
+    Callback function to send MIDI message over OSC
+    Args:
+        message: MIDI message from rtmidi. Tuple([status, data1, data2], timestamp)
+        data: osc channel. Str, eg "/midi"
+    """
+    osc_channel = data
+    midi_message = message[0] # Ignore timestamp
+    client.send_message(osc_channel, midi_message)
+    logger.info(f"Sent MIDI message {midi_message} over OSC channel {osc_channel}")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+            "--osc_channel",
+            type=str,
+            default="/midi",
+            help="The OSC channel to listen on",
+            )
+    args = parser.parse_args()
+
+    midi_in = rtmidi.MidiIn()
+    available_ports = midi_in.get_ports()
+
+    osc_channel = args.osc_channel
+
+    if available_ports:
+        midi_in.open_port(0)
+        midi_in.set_callback(send_midi_message_over_osc, osc_channel)
+        logger.info(f"Opened MIDI port {available_ports[0]}")
+    else:
+        logger.warning("No MIDI ports available. Make sure that Logic Pro X is open, and that a recording light was setup:\nLogic Pro X -> Settings -> Control Surfaces -> Setup -> New -> Recording Light")
+        logger.info("Exiting...")
+        exit(1)
+
+    logger.info(f"OSC client set up on {IP_ADDRESS}:{PORT}")
+    logger.info(f"Sending MIDI messages over OSC channel {osc_channel}")
+
+    try:
+        while True:
+            # Keep the main thread alive to receive MIDI messages
+            pass
+    except KeyboardInterrupt:
+        logger.info("Exiting...")
+    finally:
+        midi_in.close_port()
+        exit(0)
---
+++ server.py
@@ -0,0 +1,71 @@
+# This program listens to an OSC address for MIDI data
+# To be run on raspberry pi: turn on GPIO pins when recording starts, turn off when recording stops
+
+import argparse
+
+from pythonosc.dispatcher import Dispatcher
+from pythonosc import osc_server
+from loguru import logger
+
+
+def process_midi(midi_data):
+    """
+    Process MIDI data received from OSC
+    Check the MIDI message type and perform actions accordingly.
+    Args:
+        midi_data: List, MIDI message from OSC
+    """
+    status, data1, data2 = midi_data
+    if data1 == 25:
+        if data2 == 127:
+            logger.info(f"{midi_data}\tRecording started")
+        elif data2 == 0:
+            logger.info(f"{midi_data}\tRecording stopped")
+
+def midi_handler(unused_addr, args, *midi_message):
+    """
+    Callback function to handle MIDI messages
+    Args:
+        unused_addr: Unused
+        args: Additional arguments passsed via the dispatcher. Eg process_midi
+        midi_message: MIDI message from OSC, unpacked tuple
+    """
+    process_func = args[0] # Callable to process MIDI data
+    midi_data = list(midi_message) # Convert unpacked tuple to list
+    process_func(midi_data)
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+            "--ip",
+            default="127.0.0.1",
+            help="The ip to listen on",
+            )
+    parser.add_argument(
+            "--port",
+            type=int,
+            default=5005,
+            help="The port to listen on",
+            )
+    parser.add_argument(
+            "--osc_channel",
+            type=str,
+            default="/midi",
+            help="The OSC channel to listen on",
+            )
+    args = parser.parse_args()
+    dispatcher = Dispatcher()
+    dispatcher.map(
+            args.osc_channel,
+            midi_handler,
+            process_midi,
+            )
+
+    server = osc_server.ThreadingOSCUDPServer(
+            (args.ip, args.port),
+            dispatcher,
+            )
+    logger.info(f"Listening on {server.server_address}")
+    server.serve_forever()
+
